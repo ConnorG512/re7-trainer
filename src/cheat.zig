cbc8ac54faeb821fa1de9aee121971c7bf8c3a2c
Connor Garey <connorgarey1999@gmail.com>
2024-10-25 19:39:32 +0100

    Rework.

--- src/cheat.zig
+++ src/cheat.zig
@@ -2,27 +2,33 @@ const winapi = @import("winapi.zig");
 const std = @import("std");
 
 const cheatTemplate = struct {
-    addressToPatch: u64,      // Pointer to a unsigned 64 bit address
+    addressToPatch: u64,       // Pointer to a unsigned 64 bit address
     originalBytes: []const u8, // Slices of any size 
-    newBytes: []const u8,      // Slices of any size 
-    prevProtectionValue: u32,
+    newBytes: []const u8,      // Slices of any size
+    prevProtectionValue: u32, 
 
-    pub fn writeBytes(self: cheatTemplate, baseAddress:u64) void {
+    pub fn startInjection(self: cheatTemplate, baseAddress: u64) void {
+        self.byteProtection(baseAddress);
+    }
+
+    fn writeBytes(_: cheatTemplate, _:u64) void {
+        
+    }
+
+    fn byteProtection(self: cheatTemplate, baseAddress: u64) void {
+        // Calculate the base exe address + offset to the instruction 
         const calculatedAddress = baseAddress + self.addressToPatch;
+
         std.debug.print("Base address: {x}\n", .{baseAddress});
         std.debug.print("Relative Instruction: {x}\n", .{self.addressToPatch});
-        std.debug.print("Added together: {x}\n\n", .{baseAddress + self.addressToPatch});
+        std.debug.print("Added together: {x}\n\n", .{calculatedAddress});
 
-        const VirtProtResult = winapi.VirtualProtect(@ptrFromInt(calculatedAddress), 4, 0x40, null);
+        const VirtProtResult = winapi.VirtualProtect(@ptrFromInt(calculatedAddress), 4, 0x40, @constCast(&self.prevProtectionValue));
+        const GLE: winapi.DWORD = winapi.GetLastError();
 
-        if (VirtProtResult == 0) {
-            std.debug.print("ERROR: VirtualProtect Failed! Result: {d}\n", .{VirtProtResult});
-            const GLEResult = winapi.GetLastError();
-            std.debug.print("ERROR: Get Last Error Result: {d}\n", .{GLEResult});
-        }
-        else {
-            std.debug.print("Changed virtualProtect at address: {x}!\n", .{self.addressToPatch});
-            std.debug.print("VirtualProtect Result: {d}\n", .{VirtProtResult});
+        if (VirtProtResult  == 0) {
+            std.debug.print("ERROR: VirtualProtect Failed!\n", .{});
+            std.debug.print("ERROR: GetLastError = {d}", .{GLE});
         }
     }
 };
@@ -31,5 +37,5 @@ pub var infiniteScrap = cheatTemplate {
     .addressToPatch = 0x0000000001d80664,
     .originalBytes = &[_]u8 {0x48,0x8b,0x5e,0x58},
     .newBytes = &[_]u8 {0x90,0x90,0x90,0x90},
-    .prevProtectionValue = 0
+    .prevProtectionValue = 0,
 };
--- src/main.zig
+++ src/main.zig
@@ -6,13 +6,10 @@ pub export fn DllMain(_: ?winapi.HINSTANCE, fdwReason: winapi.DWORD, _: ?winapi.
     switch (fdwReason) {
         1 => { // DLL_PROCESS_ATTACH
             _ = winapi.AllocConsole();
-            // Calculating the base address of re7.exe
-            const targetName = winapi.GetModuleHandleA("re7.exe");
-            const baseAddress: u64 = @intFromPtr(targetName);
+            const baseAddress = calculateBaseAddress();
 
-            std.debug.print("DLL_PROCESS_ATTACH\n", .{});
-            std.debug.print("DLL Attached to the attached to the current process correctly...\n\n", .{});
-            cheat.infiniteScrap.writeBytes(baseAddress);
+            // Inject
+            cheat.infiniteScrap.startInjection(baseAddress);
 
             return winapi.WIN_TRUE;
         },
@@ -35,5 +32,8 @@ pub export fn DllMain(_: ?winapi.HINSTANCE, fdwReason: winapi.DWORD, _: ?winapi.
             return winapi.WIN_FALSE;
         }
     }
+}
 
+ fn calculateBaseAddress() u64 {
+    return @intFromPtr(winapi.GetModuleHandleA("re7.exe"));
 }
