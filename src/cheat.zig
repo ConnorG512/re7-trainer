02e83e6aeb79c4197ab4d6b69ef373d3e867c504
Connor Garey <connorgarey1999@gmail.com>
2024-10-25 11:45:11 +0100

    spelling fix

--- src/cheat.zig
+++ src/cheat.zig
@@ -2,8 +2,8 @@ const winapi = @import("winapi.zig");
 
 const cheatTemplate = struct {
     addressToPatch: u64,      // Pointer to a unsigned 64 bit address
-    originalBytes: []const u8, // Slices of any sice 
-    newBytes: []const u8,      // Slices of any sice 
+    originalBytes: []const u8, // Slices of any size 
+    newBytes: []const u8,      // Slices of any size 
     prevProtectionValue: u32,
 
     pub fn writeBytes(self: cheatTemplate) void {
