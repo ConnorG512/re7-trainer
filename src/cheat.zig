40f0e96cb866aed9d11cf60ad81179bc29cc3829
Connor Garey <connorgarey1999@gmail.com>
2024-10-25 11:44:59 +0100

    cheat.zig additions

--- src/cheat.zig
+++ src/cheat.zig
@@ -1,18 +1,21 @@
 const winapi = @import("winapi.zig");
 
 const cheatTemplate = struct {
-    addressToPatch: *u64,      // Pointer to a unsigned 64 bit address
+    addressToPatch: u64,      // Pointer to a unsigned 64 bit address
     originalBytes: []const u8, // Slices of any sice 
     newBytes: []const u8,      // Slices of any sice 
-    prevProtectionValue: u8,
+    prevProtectionValue: u32,
 
     pub fn writeBytes(self: cheatTemplate) void {
-        winapi.VirtualProtect(self.addressToPatch, 4, 0x40, self.prevProtectionValue);
+        var oldProtect: u32 = 0;
+        winapi.VirtualProtect(@ptrCast(&self.addressToPatch) , 4, 0x40, &oldProtect);
+        self.prevProtectionValue = oldProtect;
     }
 };
 
-pub const infiniteScrap = cheatTemplate {
-    .addressToPatch = 0x141d80664,
-    .originalBytes = .{0x48,0x8b,0x5e,0x58},
-    .newBytes = .{0x90,0x90,0x90,0x90},
+pub var infiniteScrap = cheatTemplate {
+    .addressToPatch = 0x0000000141d80664,
+    .originalBytes = &[_]u8 {0x48,0x8b,0x5e,0x58},
+    .newBytes = &[_]u8 {0x90,0x90,0x90,0x90},
+    .prevProtectionValue = 0
 };
