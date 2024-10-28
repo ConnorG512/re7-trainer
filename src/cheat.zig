b5e1921eaf04f32f66bb048c8b74d772c9ffc787
Connor Garey <connorgarey1999@gmail.com>
2024-10-28 19:11:28 +0000

    Now writing to the correct bytes

--- src/cheat.zig
+++ src/cheat.zig
@@ -2,10 +2,10 @@ const winapi = @import("winapi.zig");
 const std = @import("std");
 
 const cheatTemplate = struct {
-    offsetToPatch: u64,       // Pointer to a unsigned 64 bit address
-    originalBytes: []const u8, // Slices of any size 
-    newBytes: []const u8,      // Slices of any size
-    prevProtectionValue: u32, 
+    offsetToPatch: u64, // Pointer to a unsigned 64 bit address
+    originalBytes: []const u8, // Slices of any size
+    newBytes: []const u8, // Slices of any size
+    prevProtectionValue: u32,
 
     pub fn startInjection(self: cheatTemplate, baseAddress: u64) void {
         self.byteLengnthValidation();
@@ -14,7 +14,7 @@ const cheatTemplate = struct {
     }
 
     // Validation check to ensure that the bytes that are being exchanged are of the same length
-    fn byteLengnthValidation (self: cheatTemplate) void {
+    fn byteLengnthValidation(self: cheatTemplate) void {
         const original_bytes_num: u64 = self.originalBytes.len;
         const new_bytes_num: u64 = self.newBytes.len;
 
@@ -29,7 +29,7 @@ const cheatTemplate = struct {
     }
 
     fn byteProtection(self: cheatTemplate, baseAddress: u64) void {
-        // Calculate the base exe address + offset to the instruction 
+        // Calculate the base exe address + offset to the instruction
         const calculatedAddress = baseAddress + self.offsetToPatch;
 
         std.debug.print("Base address: {x}\n", .{baseAddress});
@@ -37,34 +37,31 @@ const cheatTemplate = struct {
         std.debug.print("Base address + offset: {x}\n", .{calculatedAddress});
 
         const VirtProtResult = winapi.VirtualProtect(@ptrFromInt(calculatedAddress), 4, 0x40, @constCast(&self.prevProtectionValue));
-        
-        if (VirtProtResult  == 0) {
+
+        if (VirtProtResult == 0) {
             const GLE: winapi.DWORD = winapi.GetLastError();
             std.debug.print("ERROR: VirtualProtect Failed! {d}\n", .{VirtProtResult});
             std.debug.print("ERROR: GetLastError = {d}\n\n", .{GLE});
-        }
-        else {
+        } else {
             std.debug.print("INFO: VirtualProtect VirtProtResult: {d}\n", .{VirtProtResult});
             std.debug.print("INFO: VirtualProtect prevProtectionValue: {x}\n\n", .{self.prevProtectionValue});
         }
     }
 
-    fn writeBytes(self: cheatTemplate, baseAddress:u64) void {
+    fn writeBytes(self: cheatTemplate, baseAddress: u64) void {
         // Setting the integer value as a pointer to a space in memory and then getting the 4 bytes at that memory address.
         const ptrToAddress: *[4]u8 = @ptrFromInt(baseAddress + self.offsetToPatch);
-        var ptrSlice: [4]u8 = ptrToAddress.*;
-        std.debug.print("ptrSlice = {x}\n", .{ptrSlice});
-        std.debug.print("ptrSlice Address = {x}\n\n", .{&ptrSlice});
-
-        for (self.newBytes) |index| {
-            ptrSlice[index] = self.newBytes[index];
-        }
+        std.debug.print("{X}\n", .{ptrToAddress});
+        ptrToAddress[0] = self.newBytes[0];
+        ptrToAddress[1] = self.newBytes[1];
+        ptrToAddress[2] = self.newBytes[2];
+        ptrToAddress[3] = self.newBytes[3];
     }
 };
 
-pub var infiniteScrap = cheatTemplate {
+pub var infiniteScrap = cheatTemplate{
     .offsetToPatch = 0x0000000001d80673,
-    .originalBytes = &[_]u8 {0x44,0x89,0x7E,0x6C},
-    .newBytes = &[_]u8 {0x90,0x90,0x90,0x90},
+    .originalBytes = &[_]u8{ 0x44, 0x89, 0x7E, 0x6C },
+    .newBytes = &[_]u8{ 0x44, 0x01, 0x7E, 0x6C },
     .prevProtectionValue = 0,
 };
