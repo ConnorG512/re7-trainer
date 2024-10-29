3b250690da63cffc006713e984fa90f587cb2928
Connor Garey <connorgarey1999@gmail.com>
2024-10-29 20:56:14 +0000

    Added and changed logs

--- src/cheat.zig
+++ src/cheat.zig
@@ -24,7 +24,7 @@ pub const CheatTemplate = struct {
 
     fn allocateVirtualMemory(self: *CheatTemplate) void {
         self.*.virtualAllocateAddress = @intFromPtr(winapi.VirtualAlloc(null, 20, 0x00001000, 0x40));
-        std.debug.print("Virtual Memory allocated at address: {X}\n", .{self.*.virtualAllocateAddress});
+        std.log.debug("Virtual Memory allocated at address: {X}\n", .{self.*.virtualAllocateAddress});
     }
 
     fn byteProtection(self: *CheatTemplate) void {
@@ -33,23 +33,29 @@ pub const CheatTemplate = struct {
 
         if (VirtProtResult == 0) {
             const GLE: winapi.DWORD = winapi.GetLastError();
-            std.debug.print("ERROR: VirtualProtect Failed! {d}\n", .{VirtProtResult});
-            std.debug.print("ERROR: GetLastError = {d}\n\n", .{GLE});
+            std.log.err("VirtualProtect Failed! {d}\n", .{VirtProtResult});
+            std.log.err("GetLastError = {d}\n\n", .{GLE});
         } else {
-            std.debug.print("INFO: VirtualProtect VirtProtResult: {d}\n\n", .{VirtProtResult});
+            std.log.debug("VirtualProtect VirtProtResult: {d}\n\n", .{VirtProtResult});
         }
     }
 
     fn writeBytes(self: *CheatTemplate) void {
         // Setting the integer value as a pointer to a space in memory and then getting the 4 bytes at that memory address.
         const ptrToAddress: *[4]u8 = @ptrFromInt(self.*.baseAddress + self.*.offsetToPatch);
-        std.debug.print("{X}\n", .{ptrToAddress});
+
+        std.log.info("Offset to patch = {X}\n", .{self.*.offsetToPatch});
+        std.log.info("Base Address + offset = {X}\n", .{self.*.baseAddress + self.*.offsetToPatch});
+        std.log.info("Combines base + offet = {X}\n\n", .{ptrToAddress});
+
         ptrToAddress[0] = self.*.initJmpInstruction[0];
         ptrToAddress[1] = self.*.initJmpInstruction[1];
         ptrToAddress[2] = self.*.initJmpInstruction[2];
         ptrToAddress[3] = self.*.initJmpInstruction[3];
 
         // Storing the bytes of memory in the VirtualAlloc Memory
+        std.log.debug("Length of self.*.newBytes.len : {d}\n\n", .{self.*.newBytes.len});
+
         const ptrToVirtAllocMem: *[10]u8 = @ptrFromInt(self.*.virtualAllocateAddress);
         var index: usize = 0;
         for (self.*.newBytes) |byte| {
@@ -57,7 +63,6 @@ pub const CheatTemplate = struct {
             index += 1;
             std.log.debug("{X}", .{byte});
         }
-        
     }
 };
 
@@ -68,8 +73,8 @@ pub var infiniteScrap = CheatTemplate{
     .baseAddress = 0x0,
     .offsetToPatch = 0x0000000001d80673,
     .virtualAllocateAddress = 0x0,
-    .originalBytes = &[_]u8{ 0x44, 0x89, 0x7E, 0x6C },              // Original bytes for if the bytes need to be reverted 
-    .initJmpInstruction = &[_]u8{0x44, 0x01, 0x7E, 0x6C},           // Bytes that will replace the original code to perform the jump instruction
+    .originalBytes = &[_]u8{ 0x44, 0x89, 0x7E, 0x6C },                                // Original bytes for if the bytes need to be reverted 
+    .initJmpInstruction = &[_]u8{0x44, 0x01, 0x7E, 0x6C},                             // Bytes that will replace the original code to perform the jump instruction
     .newBytes = &[_]u8{0xC7, 0x46, 0x6C, 0x9F, 0x86, 0x01, 0x00, 0x48, 0x85, 0xDB},   // New code to modify the executable state
     .prevProtectionValue = 0x0,
 };
