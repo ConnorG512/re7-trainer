bfa00588b506ab96c0cd0d2d09c1e4505ff9c9ce
Connor Garey <connorgarey1999@gmail.com>
2024-10-29 20:33:04 +0000

    Removed byteLengthValidation as no longer needed

--- src/cheat.zig
+++ src/cheat.zig
@@ -6,13 +6,13 @@ pub const CheatTemplate = struct {
     offsetToPatch: u64,         // Pointer to a unsigned 64 bit address
     virtualAllocateAddress: u64,
     originalBytes: []const u8,  // Slices of any size
+    initJmpInstruction: []const u8,
     newBytes: []const u8,       // Slices of any size
     prevProtectionValue: u32,
 
     pub fn startInjection(self: *CheatTemplate) void {
         self.*.storeBaseAddress();
         self.*.allocateVirtualMemory();
-        self.*.byteLengthValidation();
         self.*.byteProtection();
         self.*.writeBytes();
     }
@@ -27,21 +27,6 @@ pub const CheatTemplate = struct {
         std.debug.print("Virtual Memory allocated at address: {X}\n", .{self.*.virtualAllocateAddress});
     }
 
-    // Validation check to ensure that the bytes that are being exchanged are of the same length
-    fn byteLengthValidation(self: CheatTemplate) void {
-        const original_bytes_num: u64 = self.originalBytes.len;
-        const new_bytes_num: u64 = self.newBytes.len;
-
-        std.debug.print("INFO: length of original bytes = {d}\n", .{original_bytes_num});
-        std.debug.print("INFO: length of new bytes = {d}\n", .{new_bytes_num});
-
-        if (new_bytes_num == original_bytes_num) {
-            std.debug.print("INFO: Length Validation check passed!\n\n", .{});
-        } else {
-            std.debug.print("Validation check not passed! Bytes are not of the same length!\n", .{});
-        }
-    }
-
     fn byteProtection(self: *CheatTemplate) void {
         const calculatedAddress = self.*.baseAddress + self.*.offsetToPatch;
         const VirtProtResult = winapi.VirtualProtect(@ptrFromInt(calculatedAddress), 4, 0x40, @constCast(&self.*.prevProtectionValue));
@@ -51,7 +36,7 @@ pub const CheatTemplate = struct {
             std.debug.print("ERROR: VirtualProtect Failed! {d}\n", .{VirtProtResult});
             std.debug.print("ERROR: GetLastError = {d}\n\n", .{GLE});
         } else {
-            std.debug.print("INFO: VirtualProtect VirtProtResult: {d}\n", .{VirtProtResult});
+            std.debug.print("INFO: VirtualProtect VirtProtResult: {d}\n\n", .{VirtProtResult});
         }
     }
 
@@ -59,17 +44,20 @@ pub const CheatTemplate = struct {
         // Setting the integer value as a pointer to a space in memory and then getting the 4 bytes at that memory address.
         const ptrToAddress: *[4]u8 = @ptrFromInt(self.*.baseAddress + self.*.offsetToPatch);
         std.debug.print("{X}\n", .{ptrToAddress});
-        ptrToAddress[0] = self.newBytes[0];
-        ptrToAddress[1] = self.newBytes[1];
-        ptrToAddress[2] = self.newBytes[2];
-        ptrToAddress[3] = self.newBytes[3];
+        ptrToAddress[0] = self.*.initJmpInstruction[0];
+        ptrToAddress[1] = self.*.initJmpInstruction[1];
+        ptrToAddress[2] = self.*.initJmpInstruction[2];
+        ptrToAddress[3] = self.*.initJmpInstruction[3];
 
         // Storing the bytes of memory in the VirtualAlloc Memory
-        const ptrToVirtAllocMem: *[4]u8 = @ptrFromInt(self.*.virtualAllocateAddress);
-        ptrToVirtAllocMem[0] = self.*.newBytes[0];
-        ptrToVirtAllocMem[1] = self.*.newBytes[1];
-        ptrToVirtAllocMem[2] = self.*.newBytes[2];
-        ptrToVirtAllocMem[3] = self.*.newBytes[3];
+        const ptrToVirtAllocMem: *[10]u8 = @ptrFromInt(self.*.virtualAllocateAddress);
+        var index: usize = 0;
+        for (self.*.newBytes) |byte| {
+            ptrToVirtAllocMem[index] = byte;
+            index += 1;
+            std.log.debug("{X}", .{byte});
+        }
+        
     }
 };
 
@@ -80,7 +68,8 @@ pub var infiniteScrap = CheatTemplate{
     .baseAddress = 0x0,
     .offsetToPatch = 0x0000000001d80673,
     .virtualAllocateAddress = 0x0,
-    .originalBytes = &[_]u8{ 0x44, 0x89, 0x7E, 0x6C },
-    .newBytes = &[_]u8{ 0x44, 0x01, 0x7E, 0x6C },
+    .originalBytes = &[_]u8{ 0x44, 0x89, 0x7E, 0x6C },              // Original bytes for if the bytes need to be reverted 
+    .initJmpInstruction = &[_]u8{0x44, 0x01, 0x7E, 0x6C},           // Bytes that will replace the original code to perform the jump instruction
+    .newBytes = &[_]u8{0xC7, 0x46, 0x6C, 0x9F, 0x86, 0x01, 0x00, 0x48, 0x85, 0xDB},   // New code to modify the executable state
     .prevProtectionValue = 0x0,
 };
