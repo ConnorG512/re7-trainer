100ef0323d66eca7c58d2673fe30a39c6fc34310
Connor Garey <connorgarey1999@gmail.com>
2024-11-03 16:32:13 +0000

    writeBytes function refactor

--- src/cheat.zig
+++ src/cheat.zig
@@ -3,13 +3,13 @@ const std = @import("std");
 const mf = @import("memory_functions.zig");
 
 pub const CheatTemplate = struct {
-    baseAddress: u64,                           // Base address for the application
-    offsetToPatch: u64,                         // Pointer to a unsigned 64 bit address
-    offsetToJumpBack: u64,                      // Offset to jump back to from custom code 
+    baseAddress: u64, // Base address for the application
+    offsetToPatch: u64, // Pointer to a unsigned 64 bit address
+    offsetToJumpBack: u64, // Offset to jump back to from custom code
     virtualAllocateAddress: u64,
     virtualAllocateByteSize: winapi.SIZE_T,
-    originalBytes: []const u8,                  // Slices of any size
-    newBytes: []const u8,                       // Slices of any size
+    originalBytes: []const u8, // Slices of any size
+    newBytes: []const u8, // Slices of any size
     prevProtectionValue: u32,
     returnDistanceFromBase: u8,
 
@@ -20,7 +20,7 @@ pub const CheatTemplate = struct {
         self.*.writeBytes();
     }
 
-    fn storeBaseAddress (self: *CheatTemplate) void {
+    fn storeBaseAddress(self: *CheatTemplate) void {
         self.*.baseAddress = @intFromPtr(winapi.GetModuleHandleA("re7.exe"));
         std.debug.print("Base Address: {X}\n", .{self.*.baseAddress});
     }
@@ -38,22 +38,22 @@ pub const CheatTemplate = struct {
             const current_memory_location: u64 = initial_instruction_location + allocation_jump_distance;
 
             // Reserving the address using virtual Alloc
-            virtual_alloc_result = winapi.VirtualAlloc(@ptrFromInt(current_memory_location), self.*.virtualAllocateByteSize, winapi.MEM_RESERVE,winapi.PAGE_EXECUTE_READWRITE);
+            virtual_alloc_result = winapi.VirtualAlloc(@ptrFromInt(current_memory_location), self.*.virtualAllocateByteSize, winapi.MEM_RESERVE, winapi.PAGE_EXECUTE_READWRITE);
 
             if (virtual_alloc_result != null) {
                 // Run if VirtualAlloc succeeds in finding an address
-                std.log.info("Virtual Alloc address created successfully at address {?}, offset from instruction by {d}/{X}\n", .{virtual_alloc_result, allocation_jump_distance, allocation_jump_distance});
-                
+                std.log.info("Virtual Alloc address created successfully at address {?}, offset from instruction by {d}/{X}\n", .{ virtual_alloc_result, allocation_jump_distance, allocation_jump_distance });
+
                 // Commit the memory to Virtual Alloc
                 self.*.virtualAllocateAddress = @intFromPtr(winapi.VirtualAlloc(@constCast(virtual_alloc_result), self.*.virtualAllocateByteSize, winapi.MEM_COMMIT, winapi.PAGE_EXECUTE_READWRITE));
-                std.log.info("Attempted commit allocation at address {?}, byte size {d}, allocation type {X}, flprotect {X}. Returned value {X}", .{virtual_alloc_result, self.*.virtualAllocateByteSize, 0x00001000, 0x40, self.*.virtualAllocateAddress});
+                std.log.info("Attempted commit allocation at address {?}, byte size {d}, allocation type {X}, flprotect {X}. Returned value {X}", .{ virtual_alloc_result, self.*.virtualAllocateByteSize, 0x00001000, 0x40, self.*.virtualAllocateAddress });
                 return;
             }
 
             // Adding an extra jump distance on for next loop
             const ptr_allocation_jump_distance: *u64 = &allocation_jump_distance;
             ptr_allocation_jump_distance.* += 4096 * 4; // Add with its self
-            std.log.debug("Virtual Alloc Failed! Retrying at offset {d}/{X} from address {X}\n", .{allocation_jump_distance, allocation_jump_distance, current_memory_location});
+            std.log.debug("Virtual Alloc Failed! Retrying at offset {d}/{X} from address {X}\n", .{ allocation_jump_distance, allocation_jump_distance, current_memory_location });
             std.log.debug("Get Last Error: {d}\n\n", .{winapi.GetLastError()});
         }
 
@@ -76,67 +76,19 @@ pub const CheatTemplate = struct {
 
     fn writeBytes(self: *CheatTemplate) void {
         var index: u8 = 0;
-        var ptr_jump_instruction: *[5]u8 = undefined;
-        var address_to_jump_to: u64 = 0x0;
-        var custom_code_jump_address: u64 = 0x0;
         var relative_offset: i64 = 0x0;
 
         // Writing the custom code into memory.
-
         // Calculating the jump to the custom code
-        custom_code_jump_address = self.*.baseAddress + self.offsetToPatch;
-        address_to_jump_to = self.*.virtualAllocateAddress;
-        relative_offset = @bitCast(address_to_jump_to - (custom_code_jump_address + 5));
-        
-
-        // Writing the jump to replace the original code
-        ptr_jump_instruction = @ptrFromInt(custom_code_jump_address);
-        ptr_jump_instruction[0] = 0xE9;
-        ptr_jump_instruction[1] = @intCast(relative_offset & 0xFF);
-        ptr_jump_instruction[2] = @intCast((relative_offset >> 8) & 0xFF);
-        ptr_jump_instruction[3] = @intCast((relative_offset >> 16) & 0xFF);
-        ptr_jump_instruction[4] = @intCast((relative_offset >> 24) & 0xFF);
-
-        // Print debug
-        std.log.debug("custom_code_jump_address: {X}\n", .{custom_code_jump_address});
-        std.log.debug("custom_code_jump_address + 5: {X}", .{custom_code_jump_address + 5 });
-        std.log.debug("address_to_jump_to: {X}\n", .{address_to_jump_to});
-        std.log.debug("relative_offset: {X}", .{relative_offset});
+        relative_offset = mf.calculateRelativeOffset(self.*.virtualAllocateAddress, self.*.baseAddress + self.*.offsetToPatch);
+        mf.writeJmpToMemoryAddress(@ptrFromInt(self.*.baseAddress + self.offsetToPatch), relative_offset);
 
         // This code with write the custom instruction from self.newbytes into the allocated memory space
-        var ptr_custom_code: *[10]u8 = @ptrFromInt(self.*.virtualAllocateAddress);
-                
-        for (self.*.newBytes) |byte| {
-            ptr_custom_code[index] = byte;
-            index += 1;
-        }
-        std.log.debug("Index Count: {d}", .{index});
+        index = mf.writeCustomCodeToMemory(self.*.virtualAllocateAddress, self.*.newBytes);
 
         // Writing the jump back to the original code
-        address_to_jump_to = self.*.baseAddress + self.*.offsetToJumpBack;
-        custom_code_jump_address = self.*.virtualAllocateAddress + index;
-        relative_offset = @bitCast(address_to_jump_to -% (custom_code_jump_address + 5));
-        
-        
-
-        ptr_jump_instruction = @ptrFromInt(custom_code_jump_address);
-        ptr_jump_instruction[0] = 0xE9;
-        ptr_jump_instruction[1] = @intCast(relative_offset & 0xFF);
-        ptr_jump_instruction[2] = @intCast((relative_offset >> 8) & 0xFF);
-        ptr_jump_instruction[3] = @intCast((relative_offset >> 16) & 0xFF);
-        ptr_jump_instruction[4] = @intCast((relative_offset >> 24) & 0xFF);
-        index = 0; // Resetting index after loop
-        
-        // Print debug
-        std.log.debug("custom_code_jump_address: {X}\n", .{custom_code_jump_address});
-        std.log.debug("custom_code_jump_address + 5: {X}", .{custom_code_jump_address + 5 });
-        std.log.debug("address_to_jump_to: {X}\n", .{address_to_jump_to});
-        std.log.debug("relative_offset: {X}", .{relative_offset});
-
-        // Print info strings
-        std.log.info("Base Address: {X}\n", .{self.*.baseAddress});
-        std.log.info("Offset to patch {X}\n", .{self.*.offsetToPatch});
-        std.log.info("Virtual Alloc Address {X}\n", .{self.*.virtualAllocateAddress});
+        relative_offset = mf.calculateRelativeOffset(self.*.baseAddress + self.*.offsetToJumpBack, self.*.virtualAllocateAddress + index);
+        mf.writeJmpToMemoryAddress(@ptrFromInt(self.*.virtualAllocateAddress + index), relative_offset);
     }
 };
 
@@ -150,7 +102,7 @@ pub var infiniteScrap = CheatTemplate{
     .prevProtectionValue = 0x0,
     .virtualAllocateAddress = 0x0,
     .virtualAllocateByteSize = 18,
-    .originalBytes = &[_]u8{0x44, 0x89, 0x7E, 0x6C},                                  // Original bytes for if the bytes need to be reverted 
-    .newBytes = &[_]u8{0xC7, 0x46, 0x6C, 0x9F, 0x86, 0x01, 0x00, 0x48, 0x85, 0xDB},   // New code to modify the executable state ending with an e9 jump to add the address on the end                    
+    .originalBytes = &[_]u8{ 0x44, 0x89, 0x7E, 0x6C }, // Original bytes for if the bytes need to be reverted
+    .newBytes = &[_]u8{ 0xC7, 0x46, 0x6C, 0x9F, 0x86, 0x01, 0x00, 0x48, 0x85, 0xDB }, // New code to modify the executable state ending with an e9 jump to add the address on the end
     .returnDistanceFromBase = 7,
 };
--- src/memory_functions.zig
+++ src/memory_functions.zig
@@ -1,9 +1,38 @@
-// Write the jump instruction along with the offset to the specified memory address.
+const std = @import("std");
 
-pub fn writeJmpToMemoryAddress (memoryAddress: *[5]u8, valueToWrite: i64) void {
-    memoryAddress[0] = 0xE9; // Relative jmp to address
-    memoryAddress[1] = @intCast(valueToWrite & 0xFF);
-    memoryAddress[2] = @intCast((valueToWrite >> 8) & 0xFF);
-    memoryAddress[3] = @intCast((valueToWrite >> 16) & 0xFF);
-    memoryAddress[4] = @intCast((valueToWrite >> 24) & 0xFF);
-}   
+// Write the jump instruction along with the offset to the specified memory address.
+pub fn writeJmpToMemoryAddress(memory_address: *[5]u8, value_to_write: i64) void {
+    memory_address[0] = 0xE9; // Relative jmp opcode
+    memory_address[1] = @intCast(value_to_write & 0xFF);
+    memory_address[2] = @intCast((value_to_write >> 8) & 0xFF);
+    memory_address[3] = @intCast((value_to_write >> 16) & 0xFF);
+    memory_address[4] = @intCast((value_to_write >> 24) & 0xFF);
+
+    std.log.debug("writeJmpToMemoryAddress: memory_address = {X}\n", .{memory_address});
+}
+
+// Calculates the relative offset used with the relative jmp instruction.
+// This is required for jumping to and away from custom code
+pub fn calculateRelativeOffset(memory_address_to: u64, memory_address_from: u64) i64 {
+    const relative_offset: i64 = @bitCast(memory_address_to -% (memory_address_from + 5));
+
+    std.log.debug("calculateRelativeOffset: relative_offset = {X}\n", .{relative_offset});
+    return relative_offset;
+}
+
+// write a portion of custom code to the specified memory address.
+// ensure that this memory has been allocated, and / or read / write / execute permissions have been set.
+// returns the index of the array to be used later in jumps.
+pub fn writeCustomCodeToMemory(memory_address_to_write: u64, custom_bytes: []const u8) u8 {
+    // Getting a pointer to the location in memory to write to.
+    const ptr_to_writable_memory: [*]u8 = @ptrFromInt(memory_address_to_write);
+    var index: u8 = 0;
+
+    for (custom_bytes) |byte| {
+        ptr_to_writable_memory[index] = byte;
+        index += 1;
+    }
+
+    std.log.debug("writeCustomCodeToMemory: index count = {d}", .{index});
+    return index;
+}
