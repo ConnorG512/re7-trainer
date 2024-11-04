5f2fc1d609e21cc2e40d3a157e822724c28c0675
Connor Garey <connorgarey1999@gmail.com>
2024-11-04 12:33:03 +0000

    Removed prints from dll main

--- src/main.zig
+++ src/main.zig
@@ -13,17 +13,14 @@ pub export fn DllMain(_: ?winapi.HINSTANCE, fdwReason: winapi.DWORD, _: ?winapi.
             return winapi.WIN_TRUE;
         },
         2 => { // DLL_THREAD_ATTACH
-            std.debug.print("DLL_THREAD_ATTACH\n", .{});
             
             return winapi.WIN_TRUE;
         },
         3 => { // DLL_THREAD_DETACH
-            std.debug.print("DLL_THREAD_DETACH\n", .{});
 
             return winapi.WIN_TRUE;
         },
         0 => { // DLL_PROCESS_DETACH
-            std.debug.print("DLL_PROCESS_DETACH\n", .{});
 
             return winapi.WIN_TRUE;
         },
