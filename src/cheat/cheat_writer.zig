4eb4cb586f4ac9e50a0d504af7b11cf3e26161da
Connor Garey <connorgarey1999@gmail.com>
2024-11-19 17:45:31 +0000

    addewd original bytes

--- src/cheat/cheat_writer.zig
+++ src/cheat/cheat_writer.zig
@@ -6,6 +6,7 @@ pub const CheatWriter = struct {
     cheat_base_struct: CheatBase,
 
     custom_bytes: []const u8,
+    original_bytes: []const u8,
 
     pub fn writeBytesToMemory(self: *CheatWriter, bytes_to_write: []const u8) void {
         const ptr_to_memory: [*]u8 = @ptrFromInt(self.cheat_base_struct.process_base_id.? + self.cheat_base_struct.offset_to_patch);
